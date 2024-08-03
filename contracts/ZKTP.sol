32b424e480fd76d85e1902f9b46969efbcd347a8
PavitraAgarwal21 <agarwalpavitra3000@gmail.com>
2024-08-03 18:37:50 +0530

    format solidity code

--- contracts/ZKTP.sol
+++ contracts/ZKTP.sol
@@ -1,154 +1,185 @@
-
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/utils/Address.sol";
 import "@openzeppelin/contracts/utils/math/Math.sol";
 
-// interface for the verifier contract  
+// interface for the verifier contract
 interface IVerifier {
-     function verifyProof(
+    function verifyProof(
         uint256[2] memory a,
         uint256[2][2] memory b,
         uint256[2] memory c,
         uint256[2] memory public_input
     ) external returns (bool);
-} 
- // event  
- struct TicketEvent { 
-    address payable creator ; 
-    uint256 price ; 
-    string  eventName ;
+}
+// event
+struct TicketEvent {
+    address payable creator;
+    uint256 price;
+    string eventName;
     uint256 noOfTicketAvl;
- }
-// ticket commitment 
- struct TicketCommitment {
- address buyer ;
- uint256 ticketEventIndex ;
- bool used ; 
- }
-contract ZKTP{ 
+}
+// ticket commitment
+struct TicketCommitment {
+    address buyer;
+    uint256 ticketEventIndex;
+    bool used;
+}
 
-     using Math for uint256 ; 
-     using Address for address ;
-    
-    address payable  contractOwner ; 
-    IVerifier public verifier ; 
+contract ZKTP {
+    using Math for uint256;
+    using Address for address;
+
+    address payable contractOwner;
+    IVerifier public verifier;
     uint256 public ticketEventIndex;
-    constructor(IVerifier _verify ) {
+
+    constructor(IVerifier _verify) {
         verifier = _verify;
-        contractOwner = payable (msg.sender) ;  
-        ticketEventIndex  = 0 ;
+        contractOwner = payable(msg.sender);
+        ticketEventIndex = 0;
     }
 
-// helper function 
-    function calculateFees(uint256 ticketPrice  ) pure public  returns (uint256, uint256  ) {
-        (bool _sucess ,uint256 fees) = ticketPrice.tryDiv(100) ; // the one percent of the ticket price is the  fees 
-        (bool _sucess2,uint256 total) = ticketPrice.tryAdd(fees) ; 
-        return (fees , total) ;
-    } 
-//create event for the TicketEvent 
-mapping (uint256 => TicketEvent ) ticketEvents ;
+    // helper function
+    function calculateFees(
+        uint256 ticketPrice
+    ) public pure returns (uint256, uint256) {
+        (bool _sucess, uint256 fees) = ticketPrice.tryDiv(100); // the one percent of the ticket price is the  fees
+        (bool _sucess2, uint256 total) = ticketPrice.tryAdd(fees);
+        return (fees, total);
+    }
 
-event NewTicketEvent(
-    address indexed  creator ,
-    uint256 indexed  index ,
-    string name ,
-    uint256 availableTicket ,
-    uint256 price 
-);
+    //create event for the TicketEvent
+    mapping(uint256 => TicketEvent) ticketEvents;
 
+    event NewTicketEvent(
+        address indexed creator,
+        uint256 indexed index,
+        string name,
+        uint256 availableTicket,
+        uint256 price
+    );
 
-// create Ticket - event
-function createTicketEvent(uint256 _price , string calldata _event_name , uint256 _on_of_ticket) external   {
+    // create Ticket - event
+    function createTicketEvent(
+        uint256 _price,
+        string calldata _event_name,
+        uint256 _on_of_ticket
+    ) external {
+        ticketEventIndex += 1;
+        ticketEvents[ticketEventIndex].price = _price;
+        ticketEvents[ticketEventIndex].eventName = _event_name;
+        ticketEvents[ticketEventIndex].noOfTicketAvl = _on_of_ticket;
+        ticketEvents[ticketEventIndex].creator = payable(msg.sender);
 
-ticketEventIndex +=1 ;
-ticketEvents[ticketEventIndex].price = _price ; 
-ticketEvents[ticketEventIndex].eventName = _event_name ;
-ticketEvents[ticketEventIndex].noOfTicketAvl = _on_of_ticket ;
-ticketEvents[ticketEventIndex].creator = payable (msg.sender) ;
+        emit NewTicketEvent(
+            msg.sender,
+            ticketEventIndex,
+            _event_name,
+            _on_of_ticket,
+            _price
+        );
+    }
 
-emit NewTicketEvent(msg.sender, ticketEventIndex , _event_name, _on_of_ticket, _price);
-}
+    mapping(bytes32 => TicketCommitment) TicketCommitments;
 
+    // buy ticket
+    // inputs - commitment and eventIndex
+    // commitment is generated at the backend
+    function buyTicket(
+        bytes32 commitment,
+        uint256 eventIndex
+    ) external payable {
+        // right amount of the ticket price
+        (uint256 fees, uint256 total) = calculateFees(
+            ticketEvents[eventIndex].price
+        );
+        require(msg.value == total, "Invalid ticket price ");
 
+        // ticket is sold out
+        require(
+            ticketEvents[eventIndex].noOfTicketAvl > 0,
+            "tickets are sold out !!"
+        );
 
-mapping (bytes32 =>TicketCommitment) TicketCommitments ;
+        // ticket is not valid //
+        // case one
+        // when the commitment is already used
+        require(!TicketCommitments[commitment].used, "invalid commitment");
 
-// buy ticket  
-// inputs - commitment and eventIndex 
-// commitment is generated at the backend 
-function buyTicket(bytes32 commitment , uint256 eventIndex) external payable  {
+        // it means with this commitment ticket is created
+        TicketCommitments[commitment].used = true;
+        TicketCommitments[commitment].buyer = msg.sender;
+        TicketCommitments[commitment].ticketEventIndex = eventIndex;
+        ticketEvents[eventIndex].noOfTicketAvl -= 1;
 
-// right amount of the ticket price 
-(uint256 fees , uint256 total ) = calculateFees(ticketEvents[eventIndex].price);
-require(msg.value == total , "Invalid ticket price ") ;
+        //TODO! some custom logic
+        //case 1 sending the ticket price to the event creator when it is invalidated the ticket not just after buying of thr ticket .
+        //case 2 if it is not invldated then after the event is expire the 50-50 is given back to the ticket buyer and event creator .
 
-// ticket is sold out 
-require(ticketEvents[eventIndex].noOfTicketAvl > 0 , "tickets are sold out !!") ;
+        // sending the ticket price to event creator
+        Address.sendValue(
+            ticketEvents[eventIndex].creator,
+            ticketEvents[eventIndex].price
+        );
 
-// ticket is not valid // 
-// case one 
-        // when the commitment is already used 
-    require(!TicketCommitments[commitment].used , "invalid commitment" );
+        // sending fees to the contract creator
+        Address.sendValue(contractOwner, fees);
+    }
 
-// it means with this commitment ticket is created 
-    TicketCommitments[commitment].used = true ; 
-    TicketCommitments[commitment].buyer = msg.sender ; 
-    TicketCommitments[commitment].ticketEventIndex = eventIndex ;
-    ticketEvents[eventIndex].noOfTicketAvl -= 1 ;
+    mapping(bytes32 => bool) nullifierHashes;
 
+    // verify ticket
+    // we can verify the ticket that if the person konw the nullifierhash
+    // the nullifier is not used before
+    // commitment is also correct
+    function verifyTicket(
+        bytes32 _nullifierHash,
+        bytes32 _commitment
+    ) external view returns (bool) {
+        // if the ticket already nullified or invlidated then it will be true
+        if (nullifierHashes[_nullifierHash]) {
+            return false;
+        }
+        // if ticket is not created for this commitment
+        if (!TicketCommitments[_commitment].used) {
+            return false;
+        }
+        return true;
+    }
 
-//TODO! some custom logic 
-//case 1 sending the ticket price to the event creator when it is invalidated the ticket not just after buying of thr ticket . 
-//case 2 if it is not invldated then after the event is expire the 50-50 is given back to the ticket buyer and event creator . 
+    //   function verifyProof(uint[2] calldata _pA, uint[2][2] calldata _pB, uint[2] calldata _pC, uint[2] calldata _pubSignals) public view returns (bool) {
 
+    event InvalideTicket(bytes32 commitment);
 
-// sending the ticket price to event creator 
-   Address.sendValue(ticketEvents[eventIndex].creator , ticketEvents[eventIndex].price) ; 
-   
-   
-   // sending fees to the contract creator
-   Address.sendValue(contractOwner , fees) ;
-}
+    function invalidateTicket(
+        uint256[8] calldata _proof,
+        bytes32 _commitment,
+        bytes32 _nullifierHash
+    ) internal {
+        require(
+            !nullifierHashes[_nullifierHash],
+            "nullifier hash already used"
+        );
+        require(TicketCommitments[_commitment].used, "invalid commitment");
+        // only the event creator can invalidate the ticket
+        require(
+            ticketEvents[TicketCommitments[_commitment].ticketEventIndex]
+                .creator == msg.sender,
+            "only event creator can invalidate the ticket"
+        );
+        require(
+            verifier.verifyProof(
+                [_proof[0], _proof[1]],
+                [[_proof[2], _proof[3]], [_proof[4], _proof[5]]],
+                [_proof[6], _proof[7]],
+                [uint256(_commitment), uint256(_nullifierHash)]
+            ),
+            "Invalid proof"
+        );
 
-
-mapping (bytes32 => bool) nullifierHashes ; 
-
-// verify ticket 
-// we can verify the ticket that if the person konw the nullifierhash 
-// the nullifier is not used before 
-// commitment is also correct 
-function verifyTicket(bytes32 _nullifierHash , bytes32 _commitment ) external view returns (bool) { 
-
-// if the ticket already nullified or invlidated then it will be true 
-if (nullifierHashes[_nullifierHash]) { 
-    return false ;
-}
-// if ticket is not created for this commitment  
-if (!TicketCommitments[_commitment].used)
-{
-    return false ;
-}
-return true ; 
-}
-
-//   function verifyProof(uint[2] calldata _pA, uint[2][2] calldata _pB, uint[2] calldata _pC, uint[2] calldata _pubSignals) public view returns (bool) {
-
-event InvalideTicket(bytes32 commitment) ;
-function invalidateTicket(uint256[8] calldata _proof, bytes32 _commitment , bytes32 _nullifierHash) internal {
-require(!nullifierHashes[_nullifierHash] , "nullifier hash already used");
-require(TicketCommitments[_commitment].used , "invalid commitment") ; 
-// only the event creator can invalidate the ticket 
-require(ticketEvents[TicketCommitments[_commitment].ticketEventIndex].creator == msg.sender , "only event creator can invalidate the ticket") ;
-require (verifier.verifyProof(
-    [_proof[0], _proof[1]], 
-    [[_proof[2], _proof[3]], [_proof[4], _proof[5]]], 
-    [_proof[6], _proof[7]], 
-    [uint256(_commitment), uint256(_nullifierHash)]
-    ), "Invalid proof");
-
-nullifierHashes[_nullifierHash] = true ;
-emit InvalideTicket(_commitment) ;
+        nullifierHashes[_nullifierHash] = true;
+        emit InvalideTicket(_commitment);
     }
 }
